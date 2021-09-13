3ec056c37ff24d11cc5b3017cd425e733a6f2527
gen7777 <gen777@gmail.com>
2021-09-13 16:21:20 +0300

    Конвертация

--- Convert_Doc_pdf.ps1
+++ Convert_Doc_pdf.ps1
@@ -25,3 +25,34 @@ if (!(Test-Path $pdf_path))
     Write-Verbose "Создаём каталог $pdf_path`n" -Verbose
     New-Item -ItemType directory -Path $pdf_path | Out-Null
 }
+# Количество .doc файлов
+$count = $docs.Count
+
+# Создаём объект Word
+$word = New-Object -ComObject Word.Application
+$word.Visible = $false
+
+foreach ($file in $docs) {
+    # Номер и имя обрабатываемого файла
+    $name = $file.Name
+    ++$number
+    Write-Verbose "$name `t`t (файл $number из $count)" -Verbose
+
+    # Открываем файл
+    $doc = $word.Documents.Open($file.FullName)
+    
+    # Задаём имя и путь к pdf файлу
+    $pdf_file = $pdf_path + "\" + $doc.Name
+    
+    # Меняем расширение (иначе будет pdf-файл с расширением doc)
+    $pdf_file = [System.IO.Path]::ChangeExtension($pdf_file, '.pdf')
+    
+    # Конвертируем
+    $doc.ExportAsFixedFormat($pdf_file, 'wdExportFormatPDF', $false, 1)
+    
+    # Закрываем Файл
+    $doc.Close()
+}
+
+# Закрываем Word
+$word.Quit()
